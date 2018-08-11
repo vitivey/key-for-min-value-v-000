dcef7f8fcf28a09b8a3451d7fee6d78f3ff3a010
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-11 03:44:22 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
   current_low=nil
   key=nil
@@ -12,6 +12,7 @@ def key_for_min_value(name_hash)
     elsif quantity < current_low
       current_low=quantity
       key=name
+      binding.pry
     end
   end
   end
